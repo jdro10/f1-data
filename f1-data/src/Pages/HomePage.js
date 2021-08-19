faa6a0feca8ca4226a5515b40d788c625c4959bb
Jorge Oliveira <jorgedroliveira10@gmail.com>
2021-08-19 13:38:02 +0100

    code refactor and more real data from api

--- f1-data/src/Pages/HomePage.js
+++ f1-data/src/Pages/HomePage.js
@@ -1,65 +1,14 @@
 import React from "react";
-import GenericCard from "../components/Cards/GenericCard";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import NavBar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import DriversStandings from "../components/Stats/DriversStandings";
-import ConstructorsStandings from "../components/Stats/ConstructorsStandings";
+import LatestStats from "../components/Stats/LatestStats";
 
 const HomePage = () => {
   return (
     <div>
       <NavBar />
-      <Container>
-        <Row>
-          <Col>
-            <GenericCard
-              cardTitle="Season"
-              cardBody={<h1>2021</h1>}
-              cardHeight="15rem"
-            ></GenericCard>
-          </Col>
-          <Col>
-            <GenericCard
-              cardTitle="Last race"
-              cardBody={<h1>Hungarian Grand Prix</h1>}
-              cardHeight="15rem"
-            ></GenericCard>
-          </Col>
-          <Col>
-            <GenericCard
-              cardTitle="Round"
-              cardBody={
-                <div style={{ width: 150, height: 100 }}>
-                  <CircularProgressbar value={(11 * 100) / 23} text={"11/23"} />
-                </div>
-              }
-              cardHeight="15rem"
-            ></GenericCard>
-          </Col>
-        </Row>
-        <br />
-        <Row>
-          <Col>
-            <GenericCard
-              cardTitle="Drivers' standings"
-              cardBody={<DriversStandings />}
-              cardHeight="27rem"
-            ></GenericCard>
-          </Col>
-          <Col>
-            <GenericCard
-              cardTitle="Constructors standings"
-              cardBody={<ConstructorsStandings />}
-              cardHeight="27rem"
-            ></GenericCard>
-          </Col>
-        </Row>
-      </Container>
+      <LatestStats />
       <Footer />
     </div>
   );
---
+++ f1-data/src/components/Stats/LatestStats.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from "react";
+import GenericCard from "../Cards/GenericCard";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import DriversStandings from "./DriversStandings";
+import ConstructorsStandings from "./ConstructorsStandings";
+import Spinner from "react-bootstrap/Spinner";
+
+const LatestStats = () => {
+  const [latestRaceResult, setLatestRaceResult] = useState(null);
+  const [loadingLatestRaceResult, setLoadingLatestRaceResult] = useState(true);
+
+  useEffect(() => {
+    const fetchRaceResult = async () => {
+      const response = await fetch(
+        `http://ergast.com/api/f1/current/last/results.json`
+      );
+      const data = await response.json();
+
+      setLatestRaceResult({
+        season: data.MRData.RaceTable.season,
+        round: data.MRData.RaceTable.round,
+        raceName: data.MRData.RaceTable.Races[0].raceName,
+        date: data.MRData.RaceTable.Races[0].date,
+        circuitName: data.MRData.RaceTable.Races[0].Circuit.circuitName,
+        raceWinnerNumber: data.MRData.RaceTable.Races[0].Results[0].number,
+        raceWinnerGivenName:
+          data.MRData.RaceTable.Races[0].Results[0].Driver.givenName,
+        raceWinnerFamilyName:
+          data.MRData.RaceTable.Races[0].Results[0].Driver.familyName,
+      });
+      setLoadingLatestRaceResult(false);
+    };
+
+    fetchRaceResult();
+  }, []);
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <GenericCard
+              cardTitle={
+                loadingLatestRaceResult
+                  ? ""
+                  : "Latest race - " + latestRaceResult.date
+              }
+              cardBody={
+                <h2>
+                  {loadingLatestRaceResult ? (
+                    <Spinner animation="border" role="status">
+                      <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                  ) : (
+                    latestRaceResult.raceName +
+                    " - " +
+                    latestRaceResult.circuitName
+                  )}
+                </h2>
+              }
+              cardHeight="15rem"
+            ></GenericCard>
+          </Col>
+          <Col>
+            <GenericCard
+              cardTitle="Winner"
+              cardBody={
+                <h1>
+                  {loadingLatestRaceResult ? (
+                    <Spinner animation="border" role="status">
+                      <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                  ) : (
+                    latestRaceResult.raceWinnerNumber +
+                    " - " +
+                    latestRaceResult.raceWinnerGivenName +
+                    " " +
+                    latestRaceResult.raceWinnerFamilyName
+                  )}
+                </h1>
+              }
+              cardHeight="15rem"
+            ></GenericCard>
+          </Col>
+          <Col>
+            <GenericCard
+              cardTitle="Round"
+              cardBody={
+                loadingLatestRaceResult ? (
+                  <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                  </Spinner>
+                ) : (
+                  <div style={{ width: 150, height: 100 }}>
+                    <CircularProgressbar
+                      value={(latestRaceResult.round * 100) / 23}
+                      text={latestRaceResult.round + "/23"}
+                    />
+                  </div>
+                )
+              }
+              cardHeight="15rem"
+            ></GenericCard>
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col>
+            <GenericCard
+              cardTitle="Drivers' standings"
+              cardBody={<DriversStandings />}
+              cardHeight="27rem"
+            ></GenericCard>
+          </Col>
+          <Col>
+            <GenericCard
+              cardTitle="Constructors standings"
+              cardBody={<ConstructorsStandings />}
+              cardHeight="27rem"
+            ></GenericCard>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default LatestStats;
