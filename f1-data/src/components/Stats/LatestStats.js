ca88373bb9f9b856d9cb319ebfc01b289f61225b
Jorge Oliveira <jorgedroliveira10@gmail.com>
2021-08-22 16:48:18 +0100

    added home page next race data

--- f1-data/src/components/Stats/LatestStats.js
+++ f1-data/src/components/Stats/LatestStats.js
@@ -10,9 +10,13 @@ import ConstructorsStandings from "./ConstructorsStandings";
 import SyncLoader from "react-spinners/SyncLoader";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import Flag from "react-world-flags";
 
 const LatestStats = () => {
+  const [nextRaceData, setNextRaceData] = useState(null);
   const [latestRaceResult, setLatestRaceResult] = useState(null);
+  const [totalNumberOfRaces, setTotalNumberOfRaces] = useState(null);
+  const [loadingNextRaceData, setLoadingNextRaceData] = useState(true);
   const [loadingLatestRaceResult, setLoadingLatestRaceResult] = useState(true);
 
   useEffect(() => {
@@ -27,6 +31,9 @@ const LatestStats = () => {
         round: data.MRData.RaceTable.round,
         raceName: data.MRData.RaceTable.Races[0].raceName,
         date: data.MRData.RaceTable.Races[0].date,
+        circuitCountryCode: await fetchCountryCode(
+          data.MRData.RaceTable.Races[0].Circuit.Location.country
+        ),
         circuitName: data.MRData.RaceTable.Races[0].Circuit.circuitName,
         raceWinnerNumber: data.MRData.RaceTable.Races[0].Results[0].number,
         raceWinnerGivenName:
@@ -35,10 +42,46 @@ const LatestStats = () => {
           data.MRData.RaceTable.Races[0].Results[0].Driver.familyName,
       });
       setLoadingLatestRaceResult(false);
+      fetchNextRaceData(data.MRData.RaceTable.round);
+    };
+
+    const fetchNextRaceData = async (round) => {
+      const response = await fetch(`http://ergast.com/api/f1/current.json`);
+      const data = await response.json();
+
+      setNextRaceData({
+        round: data.MRData.RaceTable.Races[parseInt(round)].round,
+        raceName: data.MRData.RaceTable.Races[parseInt(round)].raceName,
+        circuitCountryCode: await fetchCountryCode(
+          data.MRData.RaceTable.Races[parseInt(round)].Circuit.Location.country
+        ),
+        raceCountry:
+          data.MRData.RaceTable.Races[parseInt(round)].Circuit.Location.country,
+        circuitName:
+          data.MRData.RaceTable.Races[parseInt(round)].Circuit.circuitName,
+        date: data.MRData.RaceTable.Races[parseInt(round)].date,
+        time: data.MRData.RaceTable.Races[parseInt(round)].time.substring(
+          0,
+          data.MRData.RaceTable.Races[0].time.length - 1
+        ),
+      });
+
+      setTotalNumberOfRaces(data.MRData.RaceTable.Races.length);
+
+      setLoadingNextRaceData(false);
     };
 
     fetchRaceResult();
   }, []);
+
+  const fetchCountryCode = async (circuitCountry) => {
+    const response = await fetch(
+      `https://restcountries.eu/rest/v2/name/${circuitCountry}?fullText=true`
+    );
+    const data = await response.json();
+
+    return data[0].alpha3Code;
+  };
 
   return (
     <div>
@@ -52,15 +95,21 @@ const LatestStats = () => {
                   : "Latest race - " + latestRaceResult.date
               }
               cardBody={
-                <h2>
-                  {loadingLatestRaceResult ? (
-                    <SyncLoader color="black" loading="true" size={10} />
-                  ) : (
-                    latestRaceResult.raceName +
-                    " - " +
-                    latestRaceResult.circuitName
-                  )}
-                </h2>
+                loadingLatestRaceResult ? (
+                  <SyncLoader color="black" loading="true" size={10} />
+                ) : (
+                  <div>
+                    <h2>
+                      {latestRaceResult.raceName +
+                        " - " +
+                        latestRaceResult.circuitName}
+                    </h2>
+                    <Flag
+                      code={latestRaceResult.circuitCountryCode}
+                      height="50"
+                    />
+                  </div>
+                )
               }
               cardFooter={
                 loadingLatestRaceResult ? (
@@ -83,21 +132,30 @@ const LatestStats = () => {
           </Col>
           <Col>
             <GenericCard
-              cardTitle="Winner"
-              cardBody={
-                <h1>
-                  {loadingLatestRaceResult ? (
-                    <SyncLoader color="black" loading="true" size={10} />
-                  ) : (
-                    latestRaceResult.raceWinnerNumber +
-                    " - " +
-                    latestRaceResult.raceWinnerGivenName +
-                    " " +
-                    latestRaceResult.raceWinnerFamilyName
-                  )}
-                </h1>
+              cardTitle={
+                loadingNextRaceData ? "" : "Next race - " + nextRaceData.date
               }
-              cardFooter={<Button variant="primary">Driver information</Button>}
+              cardBody={
+                loadingNextRaceData ? (
+                  <SyncLoader color="black" loading="true" size={10} />
+                ) : (
+                  <div>
+                    <h3>
+                      {nextRaceData.raceName + " - " + nextRaceData.circuitName}
+                    </h3>
+                    <Flag code={nextRaceData.circuitCountryCode} height="50" />
+                  </div>
+                )
+              }
+              cardFooter={
+                loadingNextRaceData ? (
+                  ""
+                ) : (
+                  <Link to={"/home"}>
+                    <Button variant="primary">Race information</Button>
+                  </Link>
+                )
+              }
               cardHeight="18rem"
             ></GenericCard>
           </Col>
@@ -110,8 +168,10 @@ const LatestStats = () => {
                 ) : (
                   <div style={{ width: 150, height: 100 }}>
                     <CircularProgressbar
-                      value={(latestRaceResult.round * 100) / 23}
-                      text={latestRaceResult.round + "/23"}
+                      value={
+                        (latestRaceResult.round * 100) / totalNumberOfRaces
+                      }
+                      text={latestRaceResult.round + "/" + totalNumberOfRaces}
                     />
                   </div>
                 )
