1b87591b6ea57359e0f7038182bbfc8cef2f107b
Jorge Oliveira <jorgedroliveira10@gmail.com>
2021-08-23 13:50:23 +0100

    added race classification note

--- f1-data/src/components/Stats/Classifications/RaceClassification.js
+++ f1-data/src/components/Stats/Classifications/RaceClassification.js
@@ -6,45 +6,54 @@ import Col from "react-bootstrap/Col";
 
 const RaceClassification = ({ raceClassification }) => {
   return (
-    <Table responsive="sm">
-      <thead>
-        <tr>
-          <th>Pos</th>
-          <th>Number</th>
-          <th>Driver</th>
-          <th>Team</th>
-          <th>Laps</th>
-          <th>Time/Retired</th>
-          <th>Points</th>
-        </tr>
-      </thead>
-      <tbody>
-        {raceClassification.map((driver, index) => (
-          <tr key={index}>
-            <td>{driver.position}</td>
-            <td>{driver.number}</td>
-            <td>{driver.Driver.givenName + " " + driver.Driver.familyName}</td>
-            <td>{driver.Constructor.name}</td>
-            <td>{driver.laps}</td>
-            <td>
-              {driver.status === "Finished" ? driver.Time.time : driver.status}
-            </td>
-            {driver.FastestLap && driver.FastestLap.rank === "1" ? (
-              <td>
-                <Row>
-                  <Col>{driver.points}</Col>
-                  <Col>
-                    <FaStopwatch />
-                  </Col>
-                </Row>
-              </td>
-            ) : (
-              <td>{driver.points}</td>
-            )}
+    <div>
+      <Table responsive="sm">
+        <thead>
+          <tr>
+            <th>Pos</th>
+            <th>Number</th>
+            <th>Driver</th>
+            <th>Team</th>
+            <th>Laps</th>
+            <th>Time/Retired</th>
+            <th>Points</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {raceClassification.map((driver, index) => (
+            <tr key={index}>
+              <td>{driver.position}</td>
+              <td>{driver.number}</td>
+              <td>
+                {driver.Driver.givenName + " " + driver.Driver.familyName}
+              </td>
+              <td>{driver.Constructor.name}</td>
+              <td>{driver.laps}</td>
+              <td>
+                {driver.status === "Finished"
+                  ? driver.Time.time
+                  : driver.status}
+              </td>
+              {driver.FastestLap && driver.FastestLap.rank === "1" ? (
+                <td>
+                  <Row>
+                    <Col>{driver.points}</Col>
+                    <Col>
+                      <FaStopwatch />
+                    </Col>
+                  </Row>
+                </td>
+              ) : (
+                <td>{driver.points}</td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <p>
+        <FaStopwatch /> - Fastest lap of the race
+      </p>
+    </div>
   );
 };
 
