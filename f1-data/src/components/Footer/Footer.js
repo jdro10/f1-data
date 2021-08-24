6b9cc24464d69252468bdca09c2ef24a9145d67d
Jorge Oliveira <jorgedroliveira10@gmail.com>
2021-08-24 19:00:33 +0100

    added footer disclaimer

--- f1-data/src/components/Footer/Footer.js
+++ f1-data/src/components/Footer/Footer.js
@@ -22,34 +22,68 @@ const Footer = () => {
             <GiCheckeredFlag size={40} />
           </Col>
           <Col>
-            <FaGithub size={40}/>
+            <FaGithub size={40} />
           </Col>
           <Col>
-            <FaReact size={40}/>
+            <FaReact size={40} />
           </Col>
           <Col>
-            <IoLogoJavascript size={40}/>
+            <IoLogoJavascript size={40} />
           </Col>
           <Col>
-            <FaHtml5 size={40}/>
+            <FaHtml5 size={40} />
           </Col>
           <Col>
-            <FaCss3Alt size={40}/>
+            <FaCss3Alt size={40} />
           </Col>
           <Col>
-            <BsBootstrap size={40}/>
+            <BsBootstrap size={40} />
           </Col>
           <Col>
-            <SiVisualstudiocode size={40}/>
+            <SiVisualstudiocode size={40} />
           </Col>
           <Col>
-            <IoLogoNpm size={40}/>
+            <IoLogoNpm size={40} />
           </Col>
-          
         </Row>
         <Row className="justify-content-md-center" xs="auto" id="lastRow">
           <Col>
-            <p id="author">2021 Jorge Oliveira</p>
+            <p id="author"></p>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            All racing data was obtained through{" "}
+            <a href="http://ergast.com/mrd/">Ergast API</a>.
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            Countries data was obtained through{" "}
+            <a href="https://restcountries.eu/">REST Countries API</a>.
+          </Col>
+        </Row>
+        <br />
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <h5>Disclaimer</h5>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            This website was developed for learning and educational purposes
+            only.
+          </Col>
+        </Row>
+        <br/>
+        <Row className="justify-content-md-center">
+          <Col md={7}>
+            <p>
+              This website is unofficial and is not associated in any way with
+              the Formula 1 companies. F1, FORMULA ONE, FORMULA 1, FIA FORMULA
+              ONE WORLD CHAMPIONSHIP, GRAND PRIX and related marks are trade
+              marks of Formula One Licensing B.V.
+            </p>
           </Col>
         </Row>
       </Container>
