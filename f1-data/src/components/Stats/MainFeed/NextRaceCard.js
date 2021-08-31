348732ea6a47dfa768f9c72d50aafbb3be1eb5d9
Jorge Oliveira <jorgedroliveira10@gmail.com>
2021-09-01 00:11:06 +0100

    changed countdown spacing

--- f1-data/src/components/Stats/MainFeed/NextRaceCard.js
+++ f1-data/src/components/Stats/MainFeed/NextRaceCard.js
@@ -31,19 +31,19 @@ const NextRaceCard = ({ nextRaceData }) => {
         style={{ marginTop: "1%" }}
         className="justify-content-center text-center"
       >
-        <Col xs={3} sm={2}>
+        <Col xs={3} sm={2} lg={1}>
           <h1>{days}</h1>
           <h5>days</h5>
         </Col>
-        <Col xs={3} sm={2}>
+        <Col xs={3} sm={2} lg={1}>
           <h1>{hours}</h1>
           <h5>hours</h5>
         </Col>
-        <Col xs={3} sm={2}>
+        <Col xs={3} sm={2} lg={1}>
           <h1>{minutes}</h1>
           <h5>min.</h5>
         </Col>
-        <Col xs={3} sm={2}>
+        <Col xs={3} sm={2} lg={1}>
           <h1>{seconds}</h1>
           <h5>sec.</h5>
         </Col>
