cec44f8248860d36bfb6f8bdae50d9613a51fc4d
Jorge Oliveira <jorgedroliveira10@gmail.com>
2023-09-18 13:20:16 +0100

    removed unused files

--- f1-data/src/data/PreSeasonInformation.js
+++
@@ -1,8 +0,0 @@
-export const preSeasonInformation = {
-  name: "BAHRAIN PRE-SEASON TESTING",
-  country: "bh",
-  circuit_name: "Bahrain International Circuit",
-  round: 1,
-  date: "2022-03-10",
-  time: "07:00:00",
-};
