5754e1b4c68c67a775971711b9465e4209131397
Jorge Oliveira <jorgedroliveira10@gmail.com>
2024-01-29 13:39:22 +0000

    f1-data v2.0 setup

--- f1-data/src/App.js
+++ f1-data/src/App.js
@@ -1,69 +1,25 @@
-import { useState } from "react";
-import { lazy, Suspense } from "react";
-import { Route } from "react-router-dom";
-import { Switch } from "react-router-dom";
-import { HashRouter as Router } from "react-router-dom";
-import styled, { ThemeProvider } from "styled-components";
-import { ThemeContext } from "./helpers/ThemeContext";
-import { lightTheme, darkTheme, GlobalStyles } from "./themes/Themes";
-
-const HomePage = lazy(() => import("./pages/HomePage"));
-const RaceResult = lazy(() => import("./pages/RaceResult"));
-const SeasonRaces = lazy(() => import("./pages/SeasonCalendar"));
-const ErrorPage = lazy(() => import("./pages/ErrorPage"));
-const StandingsPage = lazy(() => import("./pages/StandingsPage"));
-const DriverPage = lazy(() => import("./pages/DriverPage"));
-
-const StyledApp = styled.div`
-  color: ${(props) => props.theme.fontColor};
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") !== null
-      ? localStorage.getItem("theme")
-      : "light"
-  );
-
   return (
-    <Suspense fallback={null}>
-      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-        <GlobalStyles />
-        <StyledApp>
-          <ThemeContext.Provider value={{ theme, setTheme }}>
-            <Router basename="/">
-              <Switch>
-                <Route path="/" exact={true} component={HomePage} />
-
-                <Route
-                  path="/race/:season/:round"
-                  exact={true}
-                  component={RaceResult}
-                />
-
-                <Route
-                  path="/driver/:driverId"
-                  exact={true}
-                  component={DriverPage}
-                />
-                <Route path="/schedule" exact={true} component={SeasonRaces} />
-
-                <Route path="/error" exact={true} component={ErrorPage} />
-
-                <Route
-                  path="/standings"
-                  exact={true}
-                  component={StandingsPage}
-                />
-
-                <Route component={ErrorPage} />
-              </Switch>
-            </Router>
-          </ThemeContext.Provider>
-        </StyledApp>
-      </ThemeProvider>
-    </Suspense>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- f1-data/src/data/config.js
+++
@@ -1,4 +0,0 @@
-export const config = {
-  first_season: 1950,
-  current_season: 2024,
-};
